556de24926b50bc6b5e96418c2dd50678492f411
Desire <desireirankundawilliams@gmail.com>
2025-01-10 15:33:52 +0200

    added all packages

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import "./App.css";
+import "@mantine/core/styles.css";
+import { MantineProvider } from "@mantine/core";
+
+function App() {
+  return <MantineProvider>hello there</MantineProvider>;
+}
+
+export default App;
