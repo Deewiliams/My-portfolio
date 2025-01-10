9e1755b7cd3884aec28ea3a41b53b23ae22cbfd6
Desire <desireirankundawilliams@gmail.com>
2025-01-10 18:31:26 +0200

    cleaned the code

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import "./App.css";
 import "@mantine/core/styles.css";
 import { MantineProvider } from "@mantine/core";
 import { LandingPage } from "./pages/landingPage/LandingPage";
+import { Header } from "./component/header/Header";
 
 function App() {
   return <MantineProvider defaultColorScheme="dark">
+    <Header />
     <LandingPage />
   </MantineProvider>;
 }
--- src/pages/landingPage/LandingPage.tsx
+++ src/pages/landingPage/LandingPage.tsx
@@ -2,6 +2,7 @@
 import {
   Button,
   Container,
+  Grid,
   Group,
   Image,
   List,
@@ -11,27 +12,37 @@ import {
 } from "@mantine/core";
 // import image from './image.svg';
 import classes from "./LandingPage.module.css";
-import coder from "../../assets/coder.svg"
+import coder from "../../assets/coder.svg";
 
 export function LandingPage() {
   return (
     <Container size="md">
-      <div className={classes.inner}>
-        <div className={classes.content}>
-          <Title className={classes.title}>Innovative </Title>
-          <Title className={classes.developer}> Developer</Title>
-          <Group mt={30}>
-            <Button radius="xl" size="md" className={classes.control}>
-              Hire Me
-            </Button>
-            <Button variant="default" radius="xl" size="md" className={classes.control}>
-              Download CV
-            </Button>
-          </Group>
-        </div>
-
-        <Image src={coder} className={classes.image} />
-      </div>
+      <Grid>
+        <Grid.Col span={{ base: 12, xs: 6 }}>
+          <div className={classes.inner}>
+            <div className={classes.content}>
+              <Title className={classes.title}>Innovative </Title>
+              <Title className={classes.developer}> Developer</Title>
+              <Group mt={30}>
+                <Button radius="xl" size="md" className={classes.control}>
+                  Hire Me
+                </Button>
+                <Button
+                  variant="default"
+                  radius="xl"
+                  size="md"
+                  className={classes.control}
+                >
+                  Download CV
+                </Button>
+              </Group>
+            </div>
+          </div>
+        </Grid.Col>
+        <Grid.Col span={{ base: 12, xs: 6 }}>
+          <Image src={coder} className={classes.image} />
+        </Grid.Col>
+      </Grid>
     </Container>
   );
 }
