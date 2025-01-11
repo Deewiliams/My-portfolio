2fe103551b6f70adea98e8c7c9a440b45e1cdb25
Desire <desireirankundawilliams@gmail.com>
2025-01-12 00:48:18 +0200

    added skills page

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import { MantineProvider } from "@mantine/core";
 import { LandingPage } from "./pages/landingPage/LandingPage";
 import { Header } from "./component/header/Header";
 import { AboutUs } from "./pages/AboutUs/AboutUs";
+import { Skills } from "./pages/Skills/Skills";
 
 function App() {
   return <MantineProvider defaultColorScheme="dark">
     <Header />
     <LandingPage />
     <AboutUs />
+    <Skills />
   </MantineProvider>;
 }
 
