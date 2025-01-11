d954c7ce3f59b42ef1f1227eddaf50172796acd0
Desire <desireirankundawilliams@gmail.com>
2025-01-11 23:10:41 +0200

    cleaned the code

--- src/pages/landingPage/LandingPage.tsx
+++ src/pages/landingPage/LandingPage.tsx
@@ -55,7 +55,7 @@ import {
 } from "@mantine/core";
 // import image from "./image.svg";
 import classes from "./LandingPage.module.css";
-import coder from "../../assets/coder.svg"
+import coder from "../../assets/coder.svg";
 
 export function LandingPage() {
   return (
@@ -63,38 +63,11 @@ export function LandingPage() {
       <div className={classes.inner}>
         <div className={classes.content}>
           <Title className={classes.title}>
-            A <span className={classes.highlight}>modern</span> React <br />{" "}
-            components library
+            <span className={classes.highlight}>Innovative</span>
           </Title>
-          <Text c="dimmed" mt="md">
-            Build fully functional accessible web applications faster than ever
-            – Mantine includes more than 120 customizable components and hooks
-            to cover you in any situation
-          </Text>
-
-          <List
-            mt={30}
-            spacing="sm"
-            size="sm"
-            icon={
-              <ThemeIcon size={20} radius="xl">
-                <IconCheck size={12} stroke={1.5} />
-              </ThemeIcon>
-            }
-          >
-            <List.Item>
-              <b>TypeScript based</b> – build type safe applications, all
-              components and hooks export types
-            </List.Item>
-            <List.Item>
-              <b>Free and open source</b> – all packages have MIT license, you
-              can use Mantine in any project
-            </List.Item>
-            <List.Item>
-              <b>No annoying focus ring</b> – focus ring will appear only when
-              user navigates with keyboard
-            </List.Item>
-          </List>
+          <Title className={classes.developer}>
+            Developer
+          </Title>
 
           <Group mt={30}>
             <Button radius="xl" size="md" className={classes.control}>
