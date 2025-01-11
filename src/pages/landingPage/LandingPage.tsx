f59706cd9097e237151cd92baced37c2bf4834b9
Desire <desireirankundawilliams@gmail.com>
2025-01-11 23:13:39 +0200

    cleaned the code

--- src/pages/landingPage/LandingPage.tsx
+++ src/pages/landingPage/LandingPage.tsx
@@ -1,59 +1,4 @@
-// import {
-//   Button,
-//   Container,
-//   Grid,
-//   Group,
-//   Image,
-//   Title,
-// } from "@mantine/core";
-// import classes from "./LandingPage.module.css";
-// import coder from "../../assets/coder.svg";
-
-// export function LandingPage() {
-//   return (
-//     <Container size="md">
-//       <Grid>
-//         <Grid.Col span={{ base: 12, xs: 6 }}>
-//           <div className={classes.inner}>
-//             <div className={classes.content}>
-//               <Title className={classes.title}>Innovative </Title>
-//               <Title className={classes.developer}> Developer</Title>
-//               <Group mt={30}>
-//                 <Button radius="xl" size="md" className={classes.control}>
-//                   Hire Me
-//                 </Button>
-//                 <Button
-//                   variant="default"
-//                   radius="xl"
-//                   size="md"
-//                   className={classes.control}
-//                 >
-//                   Download CV
-//                 </Button>
-//               </Group>
-//             </div>
-//           </div>
-//         </Grid.Col>
-//         <Grid.Col span={{ base: 12, xs: 6 }}>
-//           <Image src={coder} className={classes.image} />
-//         </Grid.Col>
-//       </Grid>
-//     </Container>
-//   );
-// }
-
-import { IconCheck } from "@tabler/icons-react";
-import {
-  Button,
-  Container,
-  Group,
-  Image,
-  List,
-  Text,
-  ThemeIcon,
-  Title,
-} from "@mantine/core";
-// import image from "./image.svg";
+import { Button, Container, Group, Image, Title } from "@mantine/core";
 import classes from "./LandingPage.module.css";
 import coder from "../../assets/coder.svg";
 
@@ -65,13 +10,11 @@ export function LandingPage() {
           <Title className={classes.title}>
             <span className={classes.highlight}>Innovative</span>
           </Title>
-          <Title className={classes.developer}>
-            Developer
-          </Title>
+          <Title className={classes.developer}>Developer</Title>
 
           <Group mt={30}>
             <Button radius="xl" size="md" className={classes.control}>
-              Get started
+              Hire Me
             </Button>
             <Button
               variant="default"
@@ -79,7 +22,7 @@ export function LandingPage() {
               size="md"
               className={classes.control}
             >
-              Source code
+              Download PDF
             </Button>
           </Group>
         </div>
