258ed05692c4a5a67d1e9e49a6ea4590861765f7
Desire <desireirankundawilliams@gmail.com>
2025-01-14 20:22:07 +0200

    cleaned the code

--- src/pages/landingPage/LandingPage.tsx
+++ src/pages/landingPage/LandingPage.tsx
@@ -1,28 +1,29 @@
-import { Button, Image, Text, TextInput, Title } from "@mantine/core";
+import { Button, Group, Image, Text, TextInput, Title } from "@mantine/core";
 // import image from "./image.svg";
 import classes from "./LandingPage.module.css";
-import coder from "../../assets/coder.svg"
+import coder from "../../assets/coder.svg";
 
 export function LandingPage() {
   return (
     <div className={classes.wrapper}>
       <div className={classes.body}>
-        <Title className={classes.title}>Wait a minute...</Title>
-        <Text fw={500} fz="lg" mb={5}>
-          Subscribe to our newsletter!
-        </Text>
-        <Text fz="sm" c="dimmed">
-          You will never miss important product updates, latest news and
-          community QA sessions. Our newsletter is once a week, every Sunday.
-        </Text>
-
-        <div className={classes.controls}>
-          <TextInput
-            placeholder="Your email"
-            classNames={{ input: classes.input, root: classes.inputWrapper }}
-          />
-          <Button className={classes.control}>Subscribe</Button>
-        </div>
+        <Title>
+          <span className={classes.highlight}>Innovative</span>
+        </Title>
+        <Title className={classes.title}>Developer</Title>
+        <Group mt={30}>
+          <Button radius="xl" size="md" className={classes.controlButton}>
+            Hire Me
+          </Button>
+          <Button
+            variant="default"
+            radius="xl"
+            size="md"
+            className={classes.controlButton}
+          >
+            Download PDF
+          </Button>
+        </Group>
       </div>
       <Image src={coder} className={classes.image} />
     </div>
