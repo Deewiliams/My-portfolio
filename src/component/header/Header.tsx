5ee7b4dc0f5cfb265752c43bd44cfd99bead9bb5
Desire <desireirankundawilliams@gmail.com>
2025-01-14 23:13:38 +0200

    cleaned the code

--- src/component/header/Header.tsx
+++ src/component/header/Header.tsx
@@ -1,98 +1,96 @@
 import {
-  IconBook,
-  IconChartPie3,
+  // IconBook,
+  // IconChartPie3,
   IconChevronDown,
-  IconCode,
-  IconCoin,
-  IconFingerprint,
-  IconNotification,
+  // IconCode,
+  // IconCoin,
+  // IconFingerprint,
+  // IconNotification,
 } from "@tabler/icons-react";
 import {
   Anchor,
   Box,
   Burger,
-  Button,
+  // Button,
   Center,
-  Collapse,
+  // Collapse,
   Divider,
   Drawer,
   Group,
   HoverCard,
   ScrollArea,
-  SimpleGrid,
+  // SimpleGrid,
   Text,
-  ThemeIcon,
+  // ThemeIcon,
   UnstyledButton,
   useMantineTheme,
 } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import classes from "./Header.module.css";
 
-const mockdata = [
-  {
-    icon: IconCode,
-    title: "Open source",
-    description: "This Pokémon’s cry is very loud and distracting",
-  },
-  {
-    icon: IconCoin,
-    title: "Free for everyone",
-    description: "The fluid of Smeargle’s tail secretions changes",
-  },
-  {
-    icon: IconBook,
-    title: "Documentation",
-    description: "Yanma is capable of seeing 360 degrees without",
-  },
-  {
-    icon: IconFingerprint,
-    title: "Security",
-    description: "The shell’s rounded shape and the grooves on its.",
-  },
-  {
-    icon: IconChartPie3,
-    title: "Analytics",
-    description: "This Pokémon uses its flying ability to quickly chase",
-  },
-  {
-    icon: IconNotification,
-    title: "Notifications",
-    description: "Combusken battles with the intensely hot flames it spews",
-  },
-];
+// const mockdata = [
+//   {
+//     icon: IconCode,
+//     title: "Open source",
+//     description: "This Pokémon’s cry is very loud and distracting",
+//   },
+//   {
+//     icon: IconCoin,
+//     title: "Free for everyone",
+//     description: "The fluid of Smeargle’s tail secretions changes",
+//   },
+//   {
+//     icon: IconBook,
+//     title: "Documentation",
+//     description: "Yanma is capable of seeing 360 degrees without",
+//   },
+//   {
+//     icon: IconFingerprint,
+//     title: "Security",
+//     description: "The shell’s rounded shape and the grooves on its.",
+//   },
+//   {
+//     icon: IconChartPie3,
+//     title: "Analytics",
+//     description: "This Pokémon uses its flying ability to quickly chase",
+//   },
+//   {
+//     icon: IconNotification,
+//     title: "Notifications",
+//     description: "Combusken battles with the intensely hot flames it spews",
+//   },
+// ];
 
 export function Header() {
   const [drawerOpened, { toggle: toggleDrawer, close: closeDrawer }] =
     useDisclosure(false);
-  const [linksOpened, { toggle: toggleLinks }] = useDisclosure(false);
+  // const [linksOpened, { toggle: toggleLinks }] = useDisclosure(false);
   const theme = useMantineTheme();
 
-  const links = mockdata.map((item) => (
-    <UnstyledButton className={classes.subLink} key={item.title}>
-      <Group wrap="nowrap" align="flex-start">
-        <ThemeIcon size={34} variant="default" radius="md">
-          <item.icon size={22} color={theme.colors.blue[6]} />
-        </ThemeIcon>
-        <div>
-          <Text size="sm" fw={500}>
-            {item.title}
-          </Text>
-          <Text size="xs" c="dimmed">
-            {item.description}
-          </Text>
-        </div>
-      </Group>
-    </UnstyledButton>
-  ));
+  // const links = mockdata.map((item) => (
+  //   <UnstyledButton className={classes.subLink} key={item.title}>
+  //     <Group wrap="nowrap" align="flex-start">
+  //       <ThemeIcon size={34} variant="default" radius="md">
+  //         <item.icon size={22} color={theme.colors.blue[6]} />
+  //       </ThemeIcon>
+  //       <div>
+  //         <Text size="sm" fw={500}>
+  //           {item.title}
+  //         </Text>
+  //         <Text size="xs" c="dimmed">
+  //           {item.description}
+  //         </Text>
+  //       </div>
+  //     </Group>
+  //   </UnstyledButton>
+  // ));
 
   return (
     <Box pb={120}>
       <header className={classes.header}>
         <Group justify="space-between" h="100%">
           {/* <MantineLogo size={30} /> */}
-         <Text fz="30px" >
-         Desire Irankunda
-         </Text>
+          <Text fz="30px">Desire Irankunda</Text>
           <Group h="100%" gap={0} visibleFrom="sm">
             <a href="#" className={classes.link}>
               Home
@@ -125,33 +123,17 @@ export function Header() {
 
                 <Divider my="sm" />
 
-                <SimpleGrid cols={2} spacing={0}>
-                  {links}
-                </SimpleGrid>
 
-                <div className={classes.dropdownFooter}>
-                  <Group justify="space-between">
-                    <div>
-                      <Text fw={500} fz="sm">
-                        Get started
-                      </Text>
-                      <Text size="xs" c="dimmed">
-                        Their food sources have decreased, and their numbers
-                      </Text>
-                    </div>
-                    <Button variant="default">Get started</Button>
-                  </Group>
-                </div>
               </HoverCard.Dropdown>
             </HoverCard>
             <a href="#" className={classes.link}>
-              Learn
+              My Project
             </a>
             <a href="#" className={classes.link}>
-              Academy
+              Contact Me
             </a>
           </Group>
-          
+
           <Burger
             opened={drawerOpened}
             onClick={toggleDrawer}
@@ -175,28 +157,28 @@ export function Header() {
           <a href="#" className={classes.link}>
             Home
           </a>
-          <UnstyledButton className={classes.link} onClick={toggleLinks}>
+          {/* <UnstyledButton className={classes.link} onClick={toggleLinks}>
             <Center inline>
               <Box component="span" mr={5}>
-                Features
+                Project
               </Box>
               <IconChevronDown size={16} color={theme.colors.blue[6]} />
             </Center>
-          </UnstyledButton>
-          <Collapse in={linksOpened}>{links}</Collapse>
+          </UnstyledButton> */}
+          {/* <Collapse in={linksOpened}>{links}</Collapse> */}
           <a href="#" className={classes.link}>
-            Learn
+            My Project
           </a>
           <a href="#" className={classes.link}>
-            Academy
+            Contact Me
           </a>
 
           <Divider my="sm" />
 
-          <Group justify="center" grow pb="xl" px="md">
+          {/* <Group justify="center" grow pb="xl" px="md">
             <Button variant="default">Log in</Button>
             <Button>Sign up</Button>
-          </Group>
+          </Group> */}
         </ScrollArea>
       </Drawer>
     </Box>
