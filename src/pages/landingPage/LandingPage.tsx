dfaee5c2e664966cc8d5f389a1d88caf1d876429
Desire <desireirankundawilliams@gmail.com>
2025-01-14 17:12:19 +0200

    cleaned the code

--- src/pages/landingPage/LandingPage.tsx
+++ src/pages/landingPage/LandingPage.tsx
@@ -1,33 +1,37 @@
-import { Button, Container, Group, Image, Title } from "@mantine/core";
+import { Button, Container, Grid, Group, Image, Title } from "@mantine/core";
 import classes from "./LandingPage.module.css";
 import coder from "../../assets/coder.svg";
 
 export function LandingPage() {
   return (
-    <Container size="md">
-      <div className={classes.inner}>
-        <div className={classes.content}>
-          <Title className={classes.title}>
-            <span className={classes.highlight}>Innovative</span>
-          </Title>
-          <Title className={classes.developer}>Developer</Title>
+    <Container my="md" style={{ marginTop: "150px" }}>
+      <Grid>
+        <Grid.Col span={{ base: 12, xs: 6 }}>
+          <div className={classes.content}>
+            <Title className={classes.title}>
+              <span className={classes.highlight}>Innovative</span>
+            </Title>
+            <Title className={classes.developer}>Developer</Title>
 
-          <Group mt={30}>
-            <Button radius="xl" size="md" className={classes.control}>
-              Hire Me
-            </Button>
-            <Button
-              variant="default"
-              radius="xl"
-              size="md"
-              className={classes.control}
-            >
-              Download PDF
-            </Button>
-          </Group>
-        </div>
-        <Image src={coder} className={classes.image} />
-      </div>
+            <Group mt={30}>
+              <Button radius="xl" size="md" className={classes.control}>
+                Hire Me
+              </Button>
+              <Button
+                variant="default"
+                radius="xl"
+                size="md"
+                className={classes.control}
+              >
+                Download PDF
+              </Button>
+            </Group>
+          </div>
+        </Grid.Col>
+        <Grid.Col span={{ base: 12, xs: 6 }}>
+          <Image src={coder} className={classes.image} />
+        </Grid.Col>
+      </Grid>
     </Container>
   );
 }
