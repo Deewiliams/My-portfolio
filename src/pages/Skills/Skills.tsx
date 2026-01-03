5240f67d14b8b80f08c2df49f6df5c1d1c8f557e
Desire <desireirankundawilliams@gmail.com>
2026-01-03 20:01:45 +0200

    added my skills

--- src/pages/Skills/Skills.tsx
+++ src/pages/Skills/Skills.tsx
@@ -1,5 +1,7 @@
-import { Card, Container, Image, SimpleGrid } from "@mantine/core";
+import { Card, Container, Image, SimpleGrid, Text, Group } from "@mantine/core";
 import classes from "./Skills.module.css";
+import { TitleHead } from "../../component/Title";
+
 import js from "../../assets/js.png";
 import css from "../../assets/css.png";
 import html from "../../assets/html.png";
@@ -9,41 +11,58 @@ import git from "../../assets/git.png";
 import github from "../../assets/github.png";
 import node from "../../assets/nodejs.png";
 import mongodb from "../../assets/mongodb.svg";
-import reactNavtive from "../../assets/reactNative.png";
+import reactNative from "../../assets/reactNative.png";
 import aws from "../../assets/aws.png";
-import bootstap from "../../assets/bootstrap.png";
-import { TitleHead } from "../../component/Title";
+import bootstrap from "../../assets/bootstrap.png";
 
-const mockdata = [
-  html,
-  css,
-  js,
-  react,
-  typescript,
-  github,
-  node,
-  mongodb,
-  reactNavtive,
-  aws,
-  git,
-  bootstap,
+const skills = [
+  { name: "HTML", image: html },
+  { name: "CSS", image: css },
+  { name: "JavaScript", image: js },
+  { name: "React", image: react },
+  { name: "TypeScript", image: typescript },
+  { name: "Git", image: git },
+  { name: "GitHub", image: github },
+  { name: "Node.js", image: node },
+  { name: "MongoDB", image: mongodb },
+  { name: "React Native", image: reactNative },
+  { name: "AWS", image: aws },
+  { name: "Bootstrap", image: bootstrap },
 ];
 
 export function Skills() {
   return (
     <>
       <TitleHead title="My Skills" />
+
       <Container size="lg" py="xl">
-        <SimpleGrid cols={{ base: 1, md: 4 }} spacing="xl" mt={-80}>
-          {mockdata.map((feature) => (
+        <Text ta="center" c="dimmed" mb="xl">
+          Technologies and tools I use to build modern, scalable, and
+          user-friendly applications.
+        </Text>
+
+        <SimpleGrid cols={{ base: 2, sm: 3, md: 4 }} spacing="xl">
+          {skills.map((skill) => (
             <Card
-              key={feature}
+              key={skill.name}
               shadow="md"
               radius="md"
-              className={classes.card}
               padding="xl"
+              className={classes.card}
             >
-              <Image src={feature} alt="image" width={200} height={200} />
+              <Group justify="center">
+                <Image
+                  src={skill.image}
+                  alt={skill.name}
+                  width={80}
+                  height={80}
+                  fit="contain"
+                />
+              </Group>
+
+              <Text ta="center" mt="md" fw={600}>
+                {skill.name}
+              </Text>
             </Card>
           ))}
         </SimpleGrid>
